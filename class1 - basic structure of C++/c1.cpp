8a466232a97536deab03c59f91217e99b5a0098a
Felipe <felipemeirelessantana@gmail.com>
2023-09-03 16:43:16 -0300

    finishing class 1

---
+++ class1 - basic structure of C++/c1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "Felipe Meireles is a Developer" << endl;
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl;
+    cout << "" << endl;
+
+    cout << "Lorem ipsum dolor sit amet. Abquaerat \ncommodi est exercitationem voluptas id\ndolore nesciunt!Quo omnis odio At corporis quas ut voluptates autem aut voluptates temporibus id quae dolores et cupiditate earum. \nAut accusamus officia ut consectetur dolores qui sint asperiores est alias sequi sit atque galisum.\n";
+
+    cout << "" << endl;
+    cout << "" << endl;
+    return 0;
+}
